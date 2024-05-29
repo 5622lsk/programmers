7fee4ecaa04b27ed0fc1d15b9a983c0f1445bf6b
임성경 <5622lsk@naver.com>
2024-05-29 14:13:07 +0900

    feat: 배열 두배 만들기

---
+++ Lv.0/배열 두배 만들기.js
@@ -0,0 +1,14 @@
+/* Q. 정수 배열 numbers가 매개변수로 주어집니다.
+numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(numbers) {
+  return numbers.map((number) => number * 2);
+}
+console.log(solution([1, 2, 3, 4]));
+
+//다른사람 풀이
+
+function solution2(numbers) {
+  return numbers.reduce((a, b) => [...a, b * 2], []);
+}
