e5a9a140f6d8d7c44f1d7230dbd7dbd9e057843c
임성경 <5622lsk@naver.com>
2024-05-30 12:43:42 +0900

    Day.4

---
+++ Lv.0/배열의 평균값.js
@@ -0,0 +1,7 @@
+/* 정수 배열 numbers가 매개변수로 주어집니다.
+numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요. */
+
+function solution(numbers) {
+  const sum = numbers.reduce((acc, curr) => acc + curr); //배열의 합
+  return sum / numbers.length;
+}
