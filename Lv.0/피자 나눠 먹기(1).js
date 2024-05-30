55349193e09cb9ee86debad10c761e4f0b5872d7
임성경 <5622lsk@naver.com>
2024-05-30 10:22:26 +0900

    feat: 피자나눠먹기(1)

---
+++ Lv.0/피자 나눠 먹기(1).js
@@ -0,0 +1,3 @@
+function solution(n) {
+  return Math.ceil(n / 7); //올림함수 사용
+}
---
+++ Lv.0/홀수만 담긴 배열 구하기.js
@@ -0,0 +1,14 @@
+/* 정수 n이 매개변수로 주어질 때,
+n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요. */
+function solution(n) {
+  let result = [];
+  for (let i = 1; i <= n; i += 2) {
+    result.push(i);
+  }
+  return result;
+}
+console.log(solution(30));
+
+//다른 풀이
+const solution2 = (n) =>
+  Array.from({ length: n }, (_, i) => i + 1).filter((i) => i % 2 !== 0);
