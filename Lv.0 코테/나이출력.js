36ca17c46e8637305893c9ea1bb122ec603f4ab2
임성경 <5622lsk@naver.com>
2024-05-31 15:09:31 +0900

    Day.5

---
+++ Lv.0 코테/나이출력.js
@@ -0,0 +1,9 @@
+/* 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+2022년 기준 선생님의 나이 age가 주어질 때,
+선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요 */
+
+function solution(age) {
+  const birthYear = 2022 - age + 1;
+  return birthYear;
+}
+//태어난 연도는 +1 해줘야하는걸 깜박했네,,ㅎ
