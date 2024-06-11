bda263f2958bca7c9181e926467e0d02f21a0be1
임성경 <5622lsk@naver.com>
2024-06-11 16:28:08 +0900

    Day.11

---
+++ Lv.0 코테/합성수 찾기.js
@@ -0,0 +1,37 @@
+/* 문제 설명
+약수의 개수가 세 개 이상인 수를 합성수라고 합니다.
+자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 100
+ */
+function solution(n) {
+  let 합성수 = 0;
+  for (let i = 0; i <= n; i++) {
+    let 약수개수 = 0;
+    for (let j = 0; j <= i; j++) {
+      if (i % j === 0) {
+        약수개수++;
+      }
+    }
+
+    if (약수개수 >= 3) {
+      합성수++;
+    }
+  }
+  return 합성수;
+}
+
+//다른풀이
+function solution2(n) {
+  return Array(n)
+    .fill()
+    .map((_, i) => i + 1)
+    .filter((i) => {
+      let cnt = 0;
+      for (let j = 1; j <= i; j++) {
+        if (i % j === 0) cnt++;
+      }
+      return cnt >= 3;
+    }).length;
+}
