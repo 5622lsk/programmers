fc043a6e9769510583de466a6804b27f124d4ddf
임성경 <5622lsk@naver.com>
2024-10-09 12:44:10 +0900

    Day.2

---
+++ Lv.0 알고리즘/분수의 덧셈.py
@@ -0,0 +1,10 @@
+""" 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요. """
+
+import math
+
+def solution(denum1, num1, denum2, num2):
+    denum = denum1 * num2 + denum2 * num1
+    num = num1 * num2
+    gcd = math.gcd(denum, num)
+    return [denum//gcd, num//gcd]
