7e2084b46b355dec5aae7259a9db7ac83dafb0c3
임성경 <5622lsk@naver.com>
2024-10-12 23:53:32 +0900

    Day.4

---
+++ Lv.0 알고리즘/피자나눠먹기2.py
@@ -0,0 +1,13 @@
+""" 머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다.
+피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때,
+n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ n ≤ 100 """
+
+import math
+
+def solution(n):
+    pizza = abs(n * 6) // math.gcd(n, 6)
+    #pizza = math.lcm(n, 6)
+    return pizza // 6
