29c08935a97068b34ee47cb6189d7a59e0e1a3f7
임성경 <5622lsk@naver.com>
2024-10-14 01:10:21 +0900

    Day.6

--- Lv.0 알고리즘/직각삼각형 츌력.py
+++ Lv.0 알고리즘/직각삼각형 츌력.py
@@ -4,6 +4,12 @@
 제한사항
 1 ≤ n ≤ 10 """
 
-n = int(input())
-for i in range(1, n+1):
-    print("*"*i)
+def solution(num_list):
+    even=0
+    odd=0
+    for num in num_list:
+        if num%2==0:
+            even += 1
+        else:
+            odd += 1
+    return [even, odd]
