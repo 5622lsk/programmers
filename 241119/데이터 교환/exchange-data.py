de605bbf12918c78447ef22d2e410c59a01cb908
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-19 23:42:12 +0900

    Title: 데이터 교환, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 241119/데이터 교환/exchange-data.py
+++ 241119/데이터 교환/exchange-data.py
@@ -1,7 +1,14 @@
-a,b,c = 5,6,7
-b = a
-c = b
-a = c
+# 세 정수 a, b, c 초기화
+a = 5
+b = 6
+c = 7
+
+# 값 교환
+temp = a  # 임시 변수에 a 값을 저장
+a = c     # c 값을 a에 대입
+c = b     # b 값을 c에 대입
+b = temp  # 임시 변수의 값을 b에 대입
+
 print(a)
 print(b)
 print(c)
