502e3a589ae1e34e7e699d3218cecfaec79a321f
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-26 01:45:30 +0900

    Title: 특정 조건 세 정수 비교, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 241126/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -2,9 +2,9 @@
 a,b,c = map(int, input().split())
 
 if a==min(a,b,c):
-    print(1,end(" "))
+    print(1,end=" ")
 else:
-    print(0,end(" "))
+    print(0,end=" ")
 if a==b==c:
     print(1)
 else:
