47e5437ffa7dce73c09a93dd0e7d30eecf979ce1
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-25 20:33:12 +0900

    Title: 4가지 관계연산자, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241125/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().split())
+
+print(1 if a >= b else 0)  # a가 b보다 같거나 큰지
+print(1 if a > b else 0)   # a가 b보다 큰지
+print(1 if b >= a else 0)  # b가 a보다 같거나 큰지
+print(1 if b > a else 0)
