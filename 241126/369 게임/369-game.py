63d906e4f50e360f486e98481d9984b2375a6f81
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-26 22:11:32 +0900

    Title: 369 게임, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/369 게임/369-game.py
+++ 241126/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i%3==0 or '3' in str(i) or '6' in str(i) or '9' in str(i)
+    if i%3==0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
         print(0, end=" ")
     else:
         print(i, end=" ")
