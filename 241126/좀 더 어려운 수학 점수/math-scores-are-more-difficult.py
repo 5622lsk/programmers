cebd9b067d801b4f609a87aa40951b10b61d9472
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-26 13:17:49 +0900

    Title: 좀 더 어려운 수학 점수, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241126/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,12 @@
+a_m, a_e = map(int, input().split())
+b_m, b_e = map(int, input().split())
+
+if a_m>b_m:
+    print("A")
+elif a_m<b_m:
+    print("B")
+else:
+    if a_e>b_e:
+        print("A")
+    else: a_e<b_e:
+        print("B")
