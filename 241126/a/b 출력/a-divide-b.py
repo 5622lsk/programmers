dbe3c02967255c15b650673c7cd292afec543683
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-26 20:19:09 +0900

    Title: a/b 출력, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241126/a/b 출력/a-divide-b.py
+++ 241126/a/b 출력/a-divide-b.py
@@ -1,12 +1,10 @@
 # 두 정수 입력받기
 a, b = map(int, input().split())
 
-# 나눗셈 결과 계산
-result = a / b
+print(f"a//b.", end="")
 
-# 소수점 21번째 자리에서 내림 처리
-multiplied = int(result * (10 ** 21))  # 21번째 자리까지 이동 후 정수로 변환
-trimmed_result = multiplied / (10 ** 21)  # 다시 소수점 자리로 복원
-
-# 결과 출력 (소수점 21자리로 고정)
-print(f"{trimmed_result:.21f}")
+a%=b
+for _ in range(20):
+    a*=10
+    print(a//b,end="")
+    a%=b
