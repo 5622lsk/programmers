8f90f27b4ee10925bf5cd0ee42ef8aae953a75e8
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-28 01:48:14 +0900

    Title: 청소당번 정하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241128/청소당번 정하기/cleaning-numbering.py
+++ 241128/청소당번 정하기/cleaning-numbering.py
@@ -6,11 +6,11 @@ n = int(input())
 
 for _ in range(n):
     if n % 12 == 0:
-        count += 1
+        count3 += 1
     elif n % 3 == 0:
         count2 += 1
     elif n % 2 == 0:
-        count3 += 1
+        count += 1
 
 
 # 결과 출력
