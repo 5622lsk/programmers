15b0443f4d44250900c0d174cc3c8b2290af7659
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-28 02:02:04 +0900

    Title: 윤년 횟수 구하기, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241128/윤년 횟수 구하기/number-of-leap-years.py
+++ 241128/윤년 횟수 구하기/number-of-leap-years.py
@@ -1,15 +1,12 @@
 count = 0 
-count2 = 0 
 
 num = int(input())
 
 for num in range(1, n + 1):  
-    if num % 4 == 0: 
+    if num % 4 == 0:
+        if num % 100==0 and num%400=!0 :
+            continue
         count += 1
-    elif num % 100==0 and num%400=!0 :  
-        count2 += 1
-    else:  
-        count2 += 1
 
 # 결과 출력
-print(count, count2) 
+print(count) 
