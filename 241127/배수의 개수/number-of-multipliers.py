83724c8a9a2aa255c941a1bd744d21b4733e3a06
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-27 21:30:53 +0900

    Title: 배수의 개수, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241127/배수의 개수/number-of-multipliers.py
+++ 241127/배수의 개수/number-of-multipliers.py
@@ -4,13 +4,13 @@ count2 = 0
 
 for _ in range(10):
     num = int(input())
-    if num % 3 == 0:
-        count += 1
-    elif num % 5 == 0:
-        count2 +=1
-    elif num % 5==0 and num%3==0:
+    if num % 5==0 and num%3==0:
         count+=1
         count2+=1
+    elif num % 5 == 0:
+        count2 +=1
+    elif num % 3==0:
+        count +=1
 
 # 결과 출력
 print(count, count2)
