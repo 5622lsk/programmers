0a68ae899a35408389b59b2095c4a784fd908f4e
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-29 03:26:21 +0900

    Title: 1부터 곱셈하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241129/1부터 곱셈하기/multiple-from-one.py
+++ 241129/1부터 곱셈하기/multiple-from-one.py
@@ -1,11 +1,16 @@
+# 입력받기
 n = int(input())
-prod=1
 
+# 초기값 설정
+product = 1  # 곱
+last_number = 0  # 마지막으로 곱해진 숫자
+
+# 1부터 10까지 곱셈 진행
 for i in range(1, 11):
-    prod*=i
-
-    if prod>=n:
+    product *= i
+    if product >= n:  # 곱이 n 이상인 순간
+        last_number = i
         break
-    
-    print(prod)
-    
+
+# 결과 출력
+print(last_number)
