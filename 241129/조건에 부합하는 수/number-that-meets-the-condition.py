94d8a4bf9ee85e477dde0968c57045ab01ff6bbb
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-29 02:57:41 +0900

    Title: 조건에 부합하는 수, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241129/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+for i in range(1, a+1):
+    if i%2==0 and i%4!=0:
+        continue
+    elif (i//8)%2==0:
+        continue
+    elif i%7<4:
+        continue
+    
+sort = sort.i
+print(sort, end=" ")
