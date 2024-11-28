a99f7109f730659b424855a47d404323063806d4
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-29 03:04:46 +0900

    Title: 조건에 부합하는 수, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241129/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 241129/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -1,31 +1,10 @@
-# a = int(input())
+a = int(input())
 
-# for i in range(1, a+1):
-#     if i%2==0 and i%4!=0:
-#         continue
-#     elif (i//8)%2==0:
-#         continue
-#     elif i%7<4:
-#         continue
-    
-# print(sorted(i), end=" ")
-
-a = int(input())  # 정수 a 입력받기
-
-# 결과를 저장할 리스트
-result = []
-
-# 1부터 a까지 반복
-for i in range(1, a + 1):
-    if i % 2 == 0 and i % 4 != 0:  # 짝수이면서 4의 배수가 아닌 경우
+for i in range(1, a+1):
+    if i%2==0 and i%4!=0:
         continue
-    elif (i // 8) % 2 == 0:  # 8로 나눈 몫이 짝수인 경우
+    elif (i//8)%2==0:
         continue
-    elif i % 7 < 4:  # 7로 나눈 나머지가 4보다 작은 경우
+    elif i%7<4:
         continue
-    
-    # 조건을 만족하지 않는 숫자를 리스트에 추가
-    result.append(i)
-
-# 결과를 오름차순으로 출력
-print(" ".join(map(str, result)))
+    print(i, end=" ")
