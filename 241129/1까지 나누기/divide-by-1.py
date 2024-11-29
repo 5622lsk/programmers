0426b120b8b7ff44192764b9974277d679772126
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-29 17:28:24 +0900

    Title: 1까지 나누기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241129/1까지 나누기/divide-by-1.py
+++ 241129/1까지 나누기/divide-by-1.py
@@ -1,7 +1,10 @@
 n = int(input())
+count=0
 
-for i in range(1,n):
-    if n%i==1:
-        print(i)
+for i in range(1,n+1):
+    n //=i
+    count+=1
+    if n <=1:
         break
-        
+
+print(count)
