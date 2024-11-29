64d8be585cdde54150d21a7b672e534477bb8be4
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-30 01:45:06 +0900

    Title: 20대의 평균 나이, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241130/20대의 평균 나이/average-age-of-20.py
+++ 241130/20대의 평균 나이/average-age-of-20.py
@@ -2,9 +2,10 @@ cnt=0
 sum=0
 while True:
     n = int(input())
-    sum+=n
-    cnt+=1
-    print(round(sum/cnt,2))
 
     if n<20 or n>29:
         break
+    
+    sum+=n
+    cnt+=1
+    print(round(sum/cnt,2))
