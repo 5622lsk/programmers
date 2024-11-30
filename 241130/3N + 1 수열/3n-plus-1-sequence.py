27925240b2e6f110f8760b61a4981162a6d1ce6d
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-11-30 19:16:23 +0900

    Title: 3N + 1 수열, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241130/3N + 1 수열/3n-plus-1-sequence.py
+++ 241130/3N + 1 수열/3n-plus-1-sequence.py
@@ -1,16 +1,13 @@
 N = int(input())
 cnt = 0
-while True:
-    if N=1:
-        break
-        print(cnt)
-    elif: N%2==0:
+while N!=1:
+    if: N%2==0:
         N//=2
         cnt+=1
     else:
         N=N*3+1
         cnt+=1
     
-    
+print(cnt)
 
 
