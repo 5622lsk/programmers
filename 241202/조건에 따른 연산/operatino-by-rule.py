8fa690e4877686c404128c856e4efa16f9742177
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-02 21:07:18 +0900

    Title: 조건에 따른 연산, Time: 80ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241202/조건에 따른 연산/operatino-by-rule.py
+++ 241202/조건에 따른 연산/operatino-by-rule.py
@@ -1,12 +1,14 @@
 cnt=0
 n = int(input())
 
-while n>=1000:
-if n % 2 =0:
-    n = n*3+1
-    cnt+=1
-else:
-    n = n*2+2
-    cnt+=1
+while True:
+    if n>=1000:
+        break
+    if n % 2 =0:
+        n = n*3+1
+        cnt+=1
+    else:
+        n = n*2+2
+        cnt+=1
 
 print(cnt)
