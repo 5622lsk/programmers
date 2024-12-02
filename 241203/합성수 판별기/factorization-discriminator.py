2a041e06e9572208c3effc5f02d530775af7311a
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-03 02:40:59 +0900

    Title: 합성수 판별기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/합성수 판별기/factorization-discriminator.py
+++ 241203/합성수 판별기/factorization-discriminator.py
@@ -1,6 +1,14 @@
-satisfied = False
-n = int(input())
+n = int(input())  # 정수 입력
+
+# 2 이상 n-1 이하의 수로 나누어떨어지는지 확인
+is_composite = False
 for i in range(2, n):
-    if n%i==0:
-        satisfied = True
-print("C")
+    if n % i == 0:  # 나누어떨어지면 합성수
+        is_composite = True
+        break
+
+# 결과 출력
+if is_composite:
+    print("C")
+else:
+    print("N")
