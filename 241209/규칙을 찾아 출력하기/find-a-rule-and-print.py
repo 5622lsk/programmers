57aefee7285480bc5dc194fc860a5b9440e9a720
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-09 21:56:07 +0900

    Title: 규칙을 찾아 출력하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241209/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 241209/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -1,10 +1,10 @@
-n=int(input())
-for i in range(n):
-    for j in range(n):
-        if i==0 or i==n-1 or j==0 or j==n-1:
-            print('*',end=" ")
+n = int(input())
+
+for i in range(1, n + 1):  # 행 반복
+    for j in range(1, n + 1):  # 열 반복
+        # 첫 줄, 마지막 줄, 첫 열, 마지막 열, 또는 대각선 조건
+        if i == 1 or i == n or j == 1 or j == n or i == j or i + j == n + 1:
+            print("*", end=" ")
         else:
-            if i>j:
-                print('*',end=" ")
-            else:
-                print(' ',end=" ")
+            print(" ", end=" ")  # 그 외는 공백
+    print()  # 줄바꿈
