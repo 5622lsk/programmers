200d776253a050a8ff797a2a84c7683f5ae208ba
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-09 15:13:35 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241209/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1,n+1):
+    space="  "*(n-i)
+    star="@ "*i
+    print(space+star)
+for i in range(n-1,0,-1):
+    space="  "*(n-i)
+    star="@ "*i
+    print(star+space)
+    
