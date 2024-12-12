9556221df476e1c1b4601236e43a13fea3fd2687
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-13 03:51:33 +0900

    Title: 모양대로 수 출력하기 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241213/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.py
+++ 241213/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.py
@@ -1,9 +1,11 @@
-cnt=2
-str=[2,4,6,8]
+cnt=0
 
 n = int(input())
 
 for i in range(n):
     for j in range(n):
-        print(str[(i+j)%4], end=" ")
+        cnt+=2
+        if cnt>8:
+            cnt=2
+        print(cnt, end=" ")
     print()
