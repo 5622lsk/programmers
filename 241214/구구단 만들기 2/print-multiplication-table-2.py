9d721857571f8603be91c918801b6ee75a45d20c
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-14 00:20:44 +0900

    Title: 구구단 만들기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241214/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+
+for i in range(2,10,2):
+    for j in range(b,a-1,-1):
+        print(f"{j} * {i} = {j*i}", end=" ")
+
+        if j>a:
+            print("/", end=" ")
+    print()
