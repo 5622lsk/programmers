b57a11846a99f4d493f1af0e96e3065133eb9c99
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-15 04:04:17 +0900

    Title: 알파벳 출력하기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241215/알파벳 출력하기 2/print-alphabet-2.py
+++ 241215/알파벳 출력하기 2/print-alphabet-2.py
@@ -1,12 +1,11 @@
-n = int(input())
-num=ord("A")
+n = int(input())  # 정수 N 입력
+num = ord("A")  # 시작 알파벳의 ASCII 값
 
-for i in range(n):
-    print("  "*i,end="")
-    for j in range(n-i):
-        print(chr(num), end=" ")
-        num+=1
-        if num==ord("Z"):
-            num=ord("A")
-    print()
-        
+for i in range(n):  # 행 반복
+    print("  " * i, end="")  # 행 시작 공백 출력
+    for j in range(n - i):  # 열 반복 (점점 줄어드는 열 개수)
+        print(chr(num), end=" ")  # 현재 알파벳 출력
+        num += 1  # 다음 알파벳으로 이동
+        if num > ord("Z"):  # Z 다음에는 A로 순환
+            num = ord("A")
+    print()  # 줄바꿈
