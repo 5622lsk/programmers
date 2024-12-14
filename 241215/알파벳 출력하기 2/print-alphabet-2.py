8a340d75887a138473cb310bb87be2a34aeea442
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-15 04:02:22 +0900

    Title: 알파벳 출력하기 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241215/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+num=ord("A")
+
+for i in range(n):
+    print("  "*i,end=" ")
+    for j in range(n-i):
+        print(chr(num), end=" ")
+        num+=1
+        if num==ord("Z"):
+            num=ord("A")
+    print()
+        
