d58dc0a1d200418c15e1e744075bef999a70947c
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-12-17 20:54:59 +0900

    Title: 학점 계산기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241217/학점 계산기/credit-calculator.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(float, input().split()))
+sum=sum(arr)
+avg=sum/(len(arr))
+print(round(avg,1),)
+
+if avg >= 4.0:
+    print("Perfect")
+elif avg>=3.0:
+    print("Good")
+else:
+    print("Poor")
