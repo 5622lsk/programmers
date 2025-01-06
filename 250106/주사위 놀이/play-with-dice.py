c68ea192b0e87e9b33482e43f4b98c1af0ce7edf
임성경 <131395142+5622lsk@users.noreply.github.com>
2025-01-06 11:44:04 +0900

    Title: 주사위 놀이, Time: 83ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250106/주사위 놀이/play-with-dice.py
+++ 250106/주사위 놀이/play-with-dice.py
@@ -3,6 +3,6 @@ arr = list(map(int, input().split()))
 for i in arr:
     count[i-1]+=1
 
-for j in range(1,11):
+for j in range(7):
     cnt=count[j]
-    print(f"{j}" - "{cnt}")
+    print(f"{j} - {cnt}")
