621716771c88d7ac9bb14436599b2cece2dc1359
임성경 <131395142+5622lsk@users.noreply.github.com>
2025-01-08 02:06:18 +0900

    Title: 십의 자리 숫자, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250108/십의 자리 숫자/number-of-tens-digit.py
+++ 250108/십의 자리 숫자/number-of-tens-digit.py
@@ -1,10 +1,10 @@
-arr = list(map(int, input().split())) 
-count_arr=[0]*9
+arr = list(map(int, input().split()))  # 공백으로 구분된 정수 입력
+count_arr = [0] * 9  # 각 십의 자리 숫자의 개수를 저장할 배열 (1~9)
 
 for i in arr:
-    if i==0:
+    if i == 0:  # 입력이 0일 경우 종료
         break
-    count_arr[i//10-1]+=1
+    count_arr[i // 10 - 1] += 1  # 십의 자리 값에 해당하는 인덱스 증가
 
 for idx, j in enumerate(count_arr, start=1):  # 결과 출력
     print(f"{idx} - {j}")
