931c866eaffdaa034ff89b487ebaafda54b70bab
임성경 <131395142+5622lsk@users.noreply.github.com>
2025-01-22 13:33:30 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 82ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 250122/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,14 @@
+# 입력 받기
+n = int(input())
+arr = list(map(int, input().split()))
+
+result=[]
+end=n
+
+while end>0:
+    max_result=max(arr[::end])
+    max_index=index(max_result)+1
+    result.append(max_index)
+    end=max_index-1
+
+print(" ".join(map(str,result)))
