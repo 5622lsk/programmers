f72829198bc9095e02c08627a8cd6f5400880884
임성경 <131395142+5622lsk@users.noreply.github.com>
2025-01-22 13:35:48 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 56ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 250122/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -6,7 +6,7 @@ result=[]
 end=n
 
 while end>0:
-    max_result=max(arr[::end])
+    max_result=max(arr[:end])
     max_index=arr.index(max_result)+1
     result.append(max_index)
     end=max_index-1
