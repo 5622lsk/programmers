59f6b3288de898d6ac04958eba267d50c7d10734
임성경 <131395142+5622lsk@users.noreply.github.com>
2025-01-22 21:14:06 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 250122/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,15 +1,21 @@
-n = int(input())
-price = list(map(int, input().split()))
+# 입력 받기
+n = int(input())  # 자동차 가격 정보의 개수
+prices = list(map(int, input().split()))  # 자동차 가격 정보 리스트
 
-min_price= float('inf')
-max_profit=0
+# 초기값 설정
+min_price = float('inf')  # 현재까지의 최소 가격 (초기값은 무한대)
+max_profit = 0  # 최대 이익 (초기값은 0)
 
-for i in price:
-    profit=i-min_price
-    if i>max_profit:
-        max_profit=profit
+# 가격 리스트 순회
+for price in prices:
+    # 현재 가격에서 이익 계산
+    profit = price - min_price
+    # 최대 이익 갱신
+    if profit > max_profit:
+        max_profit = profit
+    # 최소 가격 갱신
+    if price < min_price:
+        min_price = price
 
-    if i<min_price:
-        min_price=i
-
+# 결과 출력
 print(max_profit)
